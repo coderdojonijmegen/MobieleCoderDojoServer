9cff9c334663d01edbee51834ac8c36edb2362a9
Bas Hopman <bas@familiehopman.net>
2019-06-10 11:29:00 +0200

    Portainer, Apache en Wordpress toegevoegd.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ update_os() {
 }
 
 install_dependencies() {
-	apt install -y apt-transport-https ca-certificates curl software-properties-common git hostapd iproute2 iw haveged dnsmasq iptables procps bash util-linux build-essential &&
+	apt install -y apt-transport-https ca-certificates curl software-properties-common git hostapd iproute2 iw haveged dnsmasq iptables procps bash util-linux build-essential python3 python3-pip &&
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" &&
 	apt update &&
@@ -32,6 +32,7 @@ install_accesspoint() {
 		sleep 10 &&
 		systemctl status create_ap &&
 	popd &&
+	rm -rf create_ap/ &&
 	return 0
 }
 
@@ -45,7 +46,41 @@ configure_firewall() {
 	return 0
 }
 
-update_os &&
-install_dependencies &&
-install_accesspoint &&
-configure_firewall
+install_portainer() {
+	docker volume create portainer_data &&
+	docker stop portainer || docker rm portainer ||
+	docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
+	pip3 install docker-compose &&
+	return 0
+}
+
+install_cockpit() {
+	apt install -y cockpit &&
+	systemctl enable cockpit.socket &&
+	return 0
+}
+
+install_wordpress() {
+	pushd install &&
+		docker-compose -f wordpress.yaml up -d &&
+	popd &&
+	return 0
+}
+
+configure_apache() {
+	cp install/coderdojoserver.conf /etc/apache2/sites-available/coderdojoserver.conf &&
+	a2dissite 000-default.conf &&
+	a2ensite coderdojoserver.conf &&
+	a2enmod proxy proxy_http proxy_wstunnel rewrite &&
+	service apache2 reload &&
+	return 0
+}
+
+#update_os &&
+#install_dependencies &&
+#install_accesspoint &&
+#configure_firewall &&
+#install_portainer &&
+#install_cockpit &&
+install_wordpress &&
+configure_apache
